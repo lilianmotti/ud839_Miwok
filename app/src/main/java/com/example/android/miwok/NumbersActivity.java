314b60b70f281fd1ece957bd482035011d92b836
LiGMA <lilianmotti@users.noreply.github.com>
2018-09-06 17:16:58 +0100

    ListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -20,8 +20,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -42,20 +45,47 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        int i=0;
-        //int sizeOfWords = words.size();
-        //while (i<sizeOfWords){
-        // for (int i; i<words.size(); i++){
-        for (String str:words){
-            //Log.v("NumbersActivity", "Word at index 0: "+i);
-            TextView wordView = new TextView(this);
-            //wordView.setText(words.get(i));
-            wordView.setText(str);
-            rootView.addView(wordView);
-            i++;
-        }
+        //LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+//        int i=0;
+//        //int sizeOfWords = words.size();
+//        //while (i<sizeOfWords){
+//        // for (int i; i<words.size(); i++){
+//        for (String str:words){
+//            //Log.v("NumbersActivity", "Word at index 0: "+i);
+//            TextView wordView = new TextView(this);
+//            //wordView.setText(words.get(i));
+//            wordView.setText(str);
+//            rootView.addView(wordView);
+//            i++;
+//        }
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
